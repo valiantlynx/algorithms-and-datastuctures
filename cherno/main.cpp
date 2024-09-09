e643145c211b13e5bd66919bec39a18e25f00f63
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-09-09 14:01:26 +0200

    first function

--- cherno/main.cpp
+++ cherno/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+void Log(const char* message) {
+    std::cout << message << std::endl;
+}
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    Log("Hello World!");
     std::cin.get();
 }
