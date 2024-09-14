0b1b23d94cf86f23110d9ce87f7abeb0837e5191
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-09-14 11:21:31 +0200

    This how tyoud use classes

--- cherno/main.cpp
+++ cherno/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-#include "Log.h"
+
+#define LOG(x) std::cout << x << std::endl
+
+class Player {
+public:
+    int x, y;
+    int speed;
+
+};
 
 int main() {
-    Log("Hello World!");
+    Player player;
+    player.x = 5;
+    player.y = 5;
+
+    LOG(player.x);
+    
     std::cin.get();
 }
