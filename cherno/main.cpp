38b58b6040011152e9e19f534698f2ac739a94c4
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-09-15 13:48:21 +0200

    classes and structs

--- cherno/main.cpp
+++ cherno/main.cpp
@@ -7,14 +7,30 @@ public:
     int x, y;
     int speed;
 
+    void Move(int xa, int ya) {
+        x += xa * speed;
+        y += ya * speed;
+    }
 };
+
+struct Data {
+    int x, y;
+
+    int Add(int x, int y) {
+        return x + y;
+    }
+};
+
 
 int main() {
     Player player;
-    player.x = 5;
-    player.y = 5;
-
+    player.Move(1000, 1000);
+    
     LOG(player.x);
+    Data data;
+    int result = data.Add(4, 3);
+
+    LOG(result);
     
     std::cin.get();
 }
